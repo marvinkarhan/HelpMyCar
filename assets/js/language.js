ffcfc357046ec22d5e926466291400f987515fcc
Marvin Karhan <1821400@stud.hs-mannheim.de>
2020-06-20 17:13:20 +0200

    new architecture img

--- assets/js/language.js
+++ assets/js/language.js
@@ -4,6 +4,9 @@ let currantLang = lang;
 document.getElementById(lang).style.color = '#FFF';
 
 let deContent = new Map([
+    ['technologies-nav', 'Technologien'],
+    ['architecture-nav', 'Architektur'],
+    ['process-nav', 'Prozess'],
     ['lading-page-content-p', 'Das HelpMyCar-System hilft Autofahrern mit einem vernetzten Auto, schnell und einfach Pannen bei einem Pannenhilfeunternehmen zu melden. Mitarbeiter dieses Unternehmens können die Pannenmeldungen auf einem Dashboard ansehen. Dabei werden die von dem vernetzten Auto übermittelte Daten über den Caruso Dataplace geholt, angezeigt und bestehende Probleme visualisiert. Dem Mitarbeiter werden die aktuell verfügbaren Service-Fahrzeuge des Unternehmens angezeigt. Mit Hilfe dieser Informationen kann der Mitarbeiter entscheiden, wie man dem Kunden am schnellsten helfen kann.'],
     ['app-introduction-p', 'Die HelpMyCar Driver App ermöglicht es Fahrern eines vernetzten Autos unkompliziert eine Panne an seinem Fahrzeug zu melden und begleitet ihn anschließend auf dem Weg zu der Behebung seiner Panne.<br> Die folgenden Bilder zeigen einen typischen Pannenhilfeprozess:'],
     ['app-content-figcaption-1', 'Nachdem der Nutzer sich eingelogged hat, kann er seine bei dem HelpMyCar-System regestrierten Fahrzeugesehen.<br><br>Er kann einen kleinen Überblick über die Panne erhalten und sich dieses Fahrzeug in einer detaillierten Detailansicht ansehen.<br><br>Mit dem Button am unteren Ende des Bildschirms kann er eine Hilfsanfrage senden.'],
@@ -32,6 +35,9 @@ let deContent = new Map([
 ]);
 
 let enContent = new Map([
+    ['technologies-nav', 'Technologies'],
+    ['architecture-nav', 'Architecture'],
+    ['process-nav', 'Process'],
     ['lading-page-content-p', 'The HelpMyCar system helps drivers with a connected car to report breakdowns to a breakdown service company quickly and easily. Employees of this company can view the breakdown reports on a dashboard. The data transmitted by the connected car is retrieved via the Caruso Dataplace, to be displayed and visualized on the dashboard. The employee is shown the company\'s currently available service vehicles. With the help of this information, the employee can decide how to help the customer most quickly.'],
     ['app-introduction-p', 'The HelpMyCar Driver App enables drivers of a connected car to easily report a breakdown of their car and then accompanies them on their way to fix the breakdown.<br>The following pictures show a typical roadside assistance process:'],
     ['app-content-figcaption-1', 'After the user has logged in, he can see his registered vehicles in the HelpMyCar system.<br><br>He can get a small overview of the breakdown of his car and can have a look at each vehicle in a detailed view.<br><br>With the button at the bottom of the screen he can send a help request.'],
